a5fabd8610db5cc2ba94d2ea54ad468e33c787e0
54rtor1 <1929sartori@gmail.com>
2022-07-22 09:40:12 -0300

    button action liked

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -9,17 +9,17 @@
         />
       </div>
       <div class="column">
-        <div class="is-flex is-align-items-center is-justify-content-space-between">
+        <div class="is-flex is-align-items-center is-justify-content-space-between">4
             <section>
               <strong>00:00:00</strong>
           </section>
-          <button class="button">
+          <button class="button" @click="iniciar">
             <span class="icon">
               <i class="fas fa-play"></i>
                 </span>
                 <span>play</span>
             </button>
-            <button class="button">
+            <button class="button" @click="finalizar">
               <span class="icon">
                 <i class="fas fa-stop"></i>
                 </span>
@@ -36,7 +36,15 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'Formulario'
+  name: 'Formulario',
+  methods: {
+    iniciar () {
+      console.log('iniciando');
+    },
+    finalizar () {
+      console.log('finalizando');
+    }
+  }
 })
 
 </script>
